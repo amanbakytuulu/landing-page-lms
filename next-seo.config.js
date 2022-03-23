30a4d8f9fa4f706986d0a3e04568fb9942a20e51
aman <bakytuulu@bk.ru>
2022-03-23 12:49:22 +0600

    changed logo OG

--- next-seo.config.js
+++ next-seo.config.js
@@ -5,25 +5,23 @@ export default {
         url: 'https://lms.codifylab.com/',
         site_name: 'CODIFY CRM LMS',
         title:'CODIFY CRM LMS',
+        description:'Codify LMS — это простая платформа управления обучением',
         images: [
             {
-                url: 'https://lms.codifylab.com/logo.png',
+                url: 'https://lms.codifylab.com/logo.jpg',
                 width: 1200,
                 height: 630,
                 alt: 'logo',
-                description:'Codify LMS — это простая платформа управления обучением'
             },
             {
-                url: 'https://lms.codifylab.com/logo.png',
-                width: 1000,
-                height: 500,
-                alt: 'logo',
-                description:'Codify LMS — это простая платформа управления обучением'
+                url: 'https://lms.codifylab.com/logo.jpg',
+                width: 900,
+                height: 900,
+                alt: 'logo'
             },
             {
-                url: 'https://lms.codifylab.com/logo.png',
-                alt: 'logo',
-                description:'Codify LMS — это простая платформа управления обучением'
+                url: 'https://lms.codifylab.com/logo.jpg',
+                alt: 'logo'
             }
         ]
     }
